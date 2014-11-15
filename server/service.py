8e7a4f4fa765b1547d812942c668774486415023
icbat <cobb.evan@gmail.com>
2014-11-15 12:32:30 -0600

    basic api skeleton in place
    open, close, getids

--- server/service.py
+++ server/service.py
@@ -1,9 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
+version = "v1"
+apiEndpoint = "/api/" + version + "/"
+
+@app.route(apiEndpoint + "doors")
+def getIds():
+ 	# TODO return json of id's
+	return "I am doors. Here me roars."
+
+@app.route(apiEndpoint + "doors/<int:door_id>/open")
+def open(door_id):
+	return "opening:  " + str(door_id)
+
+@app.route(apiEndpoint + "doors/<int:door_id>/close")
+def close(door_id):
+	return "closing:  " + str(door_id)
 
 @app.route("/")
 def hello():
 	return "Hai."
+
 
 if __name__ == "__main__":
 	app.run()
