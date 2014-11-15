fa6312168b10dd565fbed467b25c1c77a58f7344
icbat <cobb.evan@gmail.com>
2014-11-15 12:48:42 -0600

    adding a list of id's and checking on calls

--- server/service.py
+++ server/service.py
@@ -1,25 +1,32 @@
 from flask import Flask
+import json
 app = Flask(__name__)
+
 version = "v1"
 apiEndpoint = "/api/" + version + "/"
+door_ids = [1, 2]
 
 @app.route(apiEndpoint + "doors")
 def getIds():
- 	# TODO return json of id's
-	return "I am doors. Here me roars."
+	return json.dumps(door_ids)
 
 @app.route(apiEndpoint + "doors/<int:door_id>/open")
 def open(door_id):
+	if (door_id not in door_ids):
+		return json.dumps("invalid door id")
+
 	return "opening:  " + str(door_id)
 
 @app.route(apiEndpoint + "doors/<int:door_id>/close")
 def close(door_id):
+	if (door_id not in door_ids):
+		return json.dumps("invalid door id")
+
 	return "closing:  " + str(door_id)
 
 @app.route("/")
 def hello():
 	return "Hai."
-
 
 if __name__ == "__main__":
 	app.run()
