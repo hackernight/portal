345fc8fe2c4e904f190fd45665da9fdfc060936e
icbat <cobb.evan@gmail.com>
2014-11-18 17:27:03 -0600

    Updating tests to include new method def
    They don't run because of the application context? but they might some
    day!

--- tests/service_tests.py
+++ tests/service_tests.py
@@ -1,13 +1,13 @@
 import service as serv
 
 def test_doors_toggle_happyPath():
-	message = serv.open(1)
+	message = serv.toggle(1)
 	
 	assert "toggling" in message
 	assert "invalid" not in message
 
 def test_doors_toggle_negative():
-	message = serv.open(-35)
+	message = serv.toggle(-35)
 	
 	assert "toggling" not in message
 	assert "invalid" in message
